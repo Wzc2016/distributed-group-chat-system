03dbcf4364544927ad6fb07b7d9caa8a33c8483e
Wu <zecheng3@sp19-cs425-g08-01.cs.illinois.edu>
2019-02-16 20:13:47 -0600

    只看main.go，运行go build main.go，然后./main name port number

--- mp1/listen/main.go
+++ mp1/listen/main.go
@@ -6,12 +6,11 @@ import(
     "net"
     // "bufio"
     // "strings"
+    "os/exec" 
+    "bytes"
 )
 
 // a map to store the memberships in the chatroom
-var memMap map[string]string
-
-
 func main() {
     // To check the input
     checkArg()
@@ -24,57 +23,19 @@ func main() {
     targets := make([]string, len(hosts))
     for i, host := range hosts {
         result := getdns(host)
-        targets[i] = string(result) + ":" + port
+        targets[i] = result + ":" + port
     }
-    // begin listen to the port
-    msgChan := make(chan string, 10)
-    go listener(port, msgChan)
-    // begin sending message
-    messageChan := make(chan []string)
+    server := setupServer(port)
+
     
-    // return the machines that are alive
-    go checkConnectAll(targets, name, messageChan)
-    
-    suc := <-messageChan
-    
-    for _, item := range suc {
 
-        checkItem(item)
-
-        conn, err := net.Dial("tcp", item)
-        // handle the error 
-        if err != nil {
-        fmt.Println("checkConnect error")
-        os.Exit(1)
-    }
-
-    }
-
-    if err != nil {
-        fmt.Println("服务没打开")
-        os.Exit(1)
-    }
-    for {
-        var msg string
-        fmt.Scan(&msg)
-        fmt.Print("<" + name + ">" + "说:")
-        fmt.Println(msg)
-        b := []byte("<" + name + ">" + "说：" + msg)
-        conn.Write(b)
-
-        select {
-            case i := <- msgChan :
-                fmt.Println(i)
-        }
-    }
-}
-
-func listener(port string, msgChan chan string){
-    server, err := net.Listen("tcp", ":" + port)
-    errHandler(err, "Can not start server!", true)
-    conn, err := server.Accept()
+    // listen and print to the prompt 
+    inMsg := make(chan string)
+    go func() {
+        for {
+        conn, err := server.Accept()
         errHandler(err, "Can not open connection!", true)
-    for {
+        // fmt.Println("成功连接，开始读取")
         go func(conn net.Conn) {
             defer conn.Close()
             for {
@@ -83,40 +44,177 @@ func listener(port string, msgChan chan string){
                 if err != nil {
                     break
                 }
-                msgChan <- string(buff[:length])
+                inMsg <- string(buff[:length])
             }
         } (conn)
     }
+    }()
+
+    var hmsg string
+    go func(){
+        for {
+            select {
+            case hmsg = <- inMsg :
+                fmt.Println(hmsg)
+            default:
+            }
+        }
+        }()
+    
+    // begin sending message
+    // messageChan := make(chan []string)
+    // return the machines that are alive
+    // for _,target := range targets {
+    //     fmt.Println(target)
+    // }
+    // var suc []string
+    memMap := make(map[string]bool)
+    go func(){
+        for{
+            checkConnectAll(targets, name, memMap)
+        }
+    }()
+
+    // go func() {
+    //     for{
+    //         if(len(suc) >= 1){
+    //             fmt.Println(suc[0])
+    //         }
+    //     }
+    // }()
+    // deal with the failures
+    
+    // memChan := make(chan []string)
+    // checkItem(suc, memMap, memChan)
+    // leaves := <-memChan
+    // for leave := range leaves {
+    //     fmt.Println(leave + " has left")
+    // }
+    
+    outMsg := make(chan string, 3)
+    var msg string
+
+    // multicast the messages
+    go func() {
+        for{ 
+            select {
+            case msg = <- outMsg:
+                for item, _ := range memMap {
+                	// face, _ := net.InterfaceAddrs()
+                    if(len(item) > 0 && item != targets[0]){
+                        // fmt.Println(item)
+                        conn, err := net.Dial("tcp", item)
+                        // handle the error 
+                        if err != nil {
+                            fmt.Println("checkConnect error")
+                            os.Exit(1)
+                        }
+                        conn.Write([]byte(msg))
+                    }
+                }
+            default:
+            }
+
+            
+        }
+    }()
+
+    for {
+        var newMsg string
+        // fmt.Printf("%s: ", name)
+        fmt.Scan(&newMsg)
+        outMsg <- name + ": " + newMsg
+    }
 }
 
+func setupServer(port string) net.Listener {
+    server, err := net.Listen("tcp", ":" + port)
+    errHandler(err, "Can not start server!", true)
+    return server
+}
 
-func checkConnectAll(targets []string, name string, msgChan chan []string) {
-        results := make([]string, 0)
+// func listener(server net.Listener, msgChan chan string){
+//     for { 
+//         conn, err := server.Accept()
+//         // fmt.Println("hi")
+//         errHandler(err, "Can not open connection!", true)
+//         go func(conn net.Conn) {
+//             defer conn.Close()
+//             for {
+//                 buff := make([]byte, 256)
+//                 length, err := conn.Read(buff)
+//                 if err != nil {
+//                     break
+//                 }
+//                 msgChan <- string(buff[:length])
+//             }
+//         } (conn)
+//     }
+// }
+
+
+func checkConnectAll(targets []string, name string, memMap map[string]bool) {
+        // var results []string
         for _, target := range targets {
-            conChan := make(chan string)
-            go checkConnect(target, name, conChan)
-            i := <- conChan
-            append(results, i)
+            // fmt.Println("hi")
+            checkConnect(target, name, memMap)  
         }
-        msgChan <- results
 }
 
 // TODO add timestamps
-func checkConnect(target string, name string, msgChan chan string) {
+func checkConnect(target string, name string, memMap map[string]bool) {
             // fmt.Println("hi")
-            results := make([]string, 1)
-            results[0] = name
-            conn, err := net.Dial("tcp", target)
+            // results := make([]string, 0)
+            // results[0] = name
+            _, err := net.Dial("tcp", target)
             // no err
+            // errHandler(err, "Can not connect to " + target, false)
             if err != nil {
+                _, ok := memMap[target]
+                if(ok == true) {
+                    delete(memMap, target)
+                    fmt.Println(target + " has left")
+                }
                 return
             }
-            msgChan <- append(results, target)
-            return
+            _, ok := memMap[target]
+            if ok == false {
+                memMap[target] = true;
+            }
+
 }
+
+
+// Get DNS of the host
+func getdns(vm string) string{
+    cmd := exec.Command("/usr/bin/dig","+short", vm)
+    resultsBytes, err := cmd.CombinedOutput()
+    resultsBytes = bytes.Trim(resultsBytes, "\n")
+    errHandler(err,"Couldn't lookup machine address:" + vm, true )
+    return string(resultsBytes)
+}
+
 
 // TODO1: Check whether items in the addrList are in the list (or map), if not, add them into the map.
 // TODO2: Check whether there are items not in the list but in the map, put them into an array and send the array into disconnects
 func checkItem(addrList []string, memMap map[string]string, disconnects chan []string) {
 
+}
+
+// handle the error message and print to the terminal
+func errHandler(err error, message string, exit bool) {
+    if err != nil {
+        fmt.Println(message)
+        if exit {
+            os.Exit(1)
+        }
+    }
+}
+
+// check the number of args
+func checkArg() {
+    if len(os.Args) != 4  {     
+        fmt.Println("Usage: " + os.Args[0] + " <name> <port> <n>")  
+        os.Exit(0)  
+    }
 }
