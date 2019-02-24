0c84961e1ddc7295db76ef83f7831cbefad9f9e5
Zecheng Wu <zecheng3@illinois.com>
2019-02-24 15:33:04 -0600

    last submission

--- main.go
+++ main.go
@@ -10,9 +10,8 @@ import(
     "bytes"
     "encoding/json"
     "strconv"
+    "time"
 )
-
-type timeStamp map[string]int
 
 type Message struct{ 
     UserName string
@@ -30,8 +29,8 @@ func main() {
     name := os.Args[1]
     port := os.Args[2]
     numMem, _ := strconv.Atoi(os.Args[3])
-    localHost,err := os.Hostname()
-    receivedMsg := make([]timeStamp,0)
+    localHost, err := os.Hostname()
+    receivedMsg := make(map[string][]int)
     if err != nil {
         panic(err)
     }
@@ -58,11 +57,11 @@ func main() {
     go func() {
         for {
         conn, err := server.Accept()
-        // errHandler(err, "Can not open connection!", true)
+        errHandler(err, "#Can not open connection!", true)
         if err != nil {
             return
         }
-        // fmt.Println("成功连接，开始读取")
+
         go func(conn net.Conn) {
             defer conn.Close()
             for {
@@ -76,41 +75,51 @@ func main() {
                 var chatMsg Message
                 err = json.Unmarshal(inBuf[:size], &chatMsg)
                 if err!=nil{
-                    fmt.Println("Unmarshal Error:", err.Error());
+                    fmt.Println("#Unmarshal Error:", err.Error());
                     return
                 }
                 _, ok := memMap[chatMsg.Address]
-                if(ok == false || memMap[chatMsg.Address] == "not") {
+                if(ok == false || len(memMap[chatMsg.Address]) == 0) {
                     memMap[chatMsg.Address] = chatMsg.UserName
                     _, timeOk := localTimeStamp[chatMsg.Address]
-                    if(timeOk==false){
+                    if(timeOk == false){
                         localTimeStamp[chatMsg.Address] = 0
                     }
                 }
                 if(len(chatMsg.Text) != 0){
-                    for{
-                    	_, received := receivedMsg[chatMsg.TimeStamp]
-                    	if(received==false){
-                        	receivedMsg[chatMsg.TimeStamp] = true
-                    	}
-                        var toDeliver = checkDeliver(chatMsg,localTimeStamp,memMap)
-                        fmt.Println("toDeliver",toDeliver)
-                        fmt.Println("chatMsg.Timestamp:",chatMsg.TimeStamp)
-                        fmt.Println("localTimeStamp before deliver:",localTimeStamp)
-                        if toDeliver == true{
-                            fmt.Println(chatMsg.UserName + ":" + chatMsg.Text+"("+")")
-                            localTimeStamp[chatMsg.Address] = localTimeStamp[chatMsg.Address]+1
-                            fmt.Println("localTimeStamp after deliver:",localTimeStamp)
-                            break
+                        received := contains(receivedMsg[chatMsg.Address], chatMsg.TimeStamp[chatMsg.Address])
+                        if(received == true){
+                            continue
                         }
-                    }
+                        receivedMsg[chatMsg.Address] = append(receivedMsg[chatMsg.Address], chatMsg.TimeStamp[chatMsg.Address])
+                        go func(){// keep judging whether a message can be delivered or not 
+                            for {
+                                var toDeliver = checkDeliver(chatMsg, localTimeStamp, memMap)
+                                if toDeliver == true{
+                                    fmt.Println(chatMsg.UserName + ": " + chatMsg.Text)
+                                    localTimeStamp[chatMsg.Address] = localTimeStamp[chatMsg.Address] + 1
+                                    // all the tests
+                                    // fmt.Println("localTimeStamp after deliver:",localTimeStamp)
+                                    // fmt.Println("toDeliver",toDeliver)
+                                    // fmt.Println("chatMsg.Timestamp:",chatMsg.TimeStamp)
+                                    // fmt.Println("localTimeStamp before deliver:",localTimeStamp)
+                                    // fmt.Println("receivedMsg after deliver:",receivedMsg)
+                                    return
+                                }
+                            }
+                            
+                        }()
+                        // var toDeliver = checkDeliver(chatMsg, localTimeStamp, memMap)
+                        
+                        
+                    
                 }
             }
         } (conn)
     }
     }()
 
-    var hmsg string
+    var hmsg string // the message to print into the console (the receiver)
     go func(){
         for {
             select {
@@ -123,7 +132,7 @@ func main() {
         }()
     
     
-    //memInfo := make(map[string]string) //key: host, value:name
+     //key: host, value:name
     go func(){
         for{
             checkConnectAll(targets, name, memMap)
@@ -132,7 +141,7 @@ func main() {
 
     // outMsg := make(chan []byte, 3)
     var msg []byte
-    outInit := make(chan []byte)
+    outInit := make(chan []byte, 3)
 
     // multicast the messages
     go func() {
@@ -142,12 +151,12 @@ func main() {
             case msg = <- outInit:
                 for item, _ := range memMap {
                     if(len(item) > 0 && item != address){
-                        conn, err := net.Dial("tcp", item)
+                        conn, err := net.DialTimeout("tcp", item, 100 * time.Millisecond)
                         // handle the error 
                         if err != nil {
-                            fmt.Println("checkConnect error")
-                            fmt.Println("len of memmap",len(memMap))
-                            os.Exit(1)
+                            // fmt.Println("checkConnect error")
+                            // fmt.Println("len of memmap",len(memMap))
+                            continue
                         }
                         conn.Write(msg)
                     }
@@ -159,24 +168,29 @@ func main() {
 
 
     ready := 0
+    current_mem := 0
     for {
-        if(ready == 0) {
+        if(ready == 0) {// not ready
             for {
                 var chatText string
-
-                outMsg := &Message{UserName:name, Address:address, Text:chatText}
+                // var TimeStamp map[string]int
+                outMsg := &Message{UserName:name, Address:address, Text:chatText, TimeStamp:localTimeStamp}
                 b, err := json.Marshal(outMsg)
                 if err != nil {
-                    fmt.Println("encoding faild")
+                    fmt.Println("#encoding faild")
                 }
-                outInit <- b
+                if(current_mem < len(memMap)) {
+                    outInit <- b
+                    current_mem += 1
+                }
+                
                 if(len(memMap) ==  numMem){
                     ready = 1
                     fmt.Println("READY")
                     break
                 }
             }
-        } else {
+        } else {// ready
             for {
                 var chatText string
             
@@ -187,10 +201,10 @@ func main() {
                 // msgTimeStamp:= make([]int,len(localTimeStamp))
                 // copy(msgTimeStamp,localTimeStamp)
                 localTimeStamp[address] = localTimeStamp[address] + 1
-                outMsg := &Message{UserName:name, Address:address, Text:chatText,TimeStamp:localTimeStamp}
+                outMsg := &Message{UserName:name, Address:address, Text:chatText, TimeStamp:localTimeStamp}
                 b, err := json.Marshal(outMsg)
                 if err != nil {
-                    fmt.Println("encoding faild")
+                    fmt.Println("#encoding faild")
                 }
                 outInit <- b
             }
@@ -200,12 +214,7 @@ func main() {
     
 }
 
-// func getId(host string)int{
-//     fmt.Println("Test,host:",host)
-//     id,_ := strconv.Atoi(host[15:17])
-//     return id - 1
-// }
-
+// implement the algorithm of vector timestamps
 func checkDeliver(chatMsg Message,localTimeStamp map[string]int,memMap map[string]string)bool{
     var toDeliver bool = true
     fromAddress := chatMsg.Address
@@ -223,13 +232,14 @@ func checkDeliver(chatMsg Message,localTimeStamp map[string]int,memMap map[strin
     return toDeliver
 }
 
+// set up the listener
 func setupServer(port string) net.Listener {
     server, err := net.Listen("tcp", ":" + port)
-    errHandler(err, "Can not start server!", true)
+    errHandler(err, "#Can not start server!", true)
     return server
 }
 
-
+// dial all the 10 VMs to check whether they can connect successfully (run in the background) 
 func checkConnectAll(targets []string, name string, memMap map[string]string) {
         // var results []string
         for _, target := range targets {
@@ -243,20 +253,20 @@ func checkConnect(target string, name string, memMap map[string]string) {
             // fmt.Println("hi")
             // results := make([]string, 0)
             // results[0] = name
-            _, err := net.Dial("tcp", target)
+            _, err := net.DialTimeout("tcp", target, 100 * time.Millisecond)
             // no err
             // errHandler(err, "Can not connect to " + target, false)
             if err != nil {
                 _, ok := memMap[target]
-                if(ok == true) {
+                if(ok == true && len(memMap[target]) > 0) {// if cannot dial, then print left information
                     fmt.Println(memMap[target] + " has left")
                     delete(memMap, target)
                 }
                 return
             }
             _, ok := memMap[target]
-            if ok == false {
-                memMap[target] = "not";
+            if ok == false {// add the node into the memMap
+                memMap[target] = "";
             }
 
 }
@@ -266,15 +276,8 @@ func getdns(vm string) string{
     cmd := exec.Command("/usr/bin/dig","+short", vm)
     resultsBytes, err := cmd.CombinedOutput()
     resultsBytes = bytes.Trim(resultsBytes, "\n")
-    errHandler(err,"Couldn't lookup machine address:" + vm, true )
+    errHandler(err,"#Couldn't lookup machine address:" + vm, true )
     return string(resultsBytes)
-}
-
-
-// TODO1: Check whether items in the addrList are in the list (or map), if not, add them into the map.
-// TODO2: Check whether there are items not in the list but in the map, put them into an array and send the array into disconnects
-func checkItem(addrList []string, memMap map[string]string, disconnects chan []string) {
-
 }
 
 // handle the error message and print to the terminal
@@ -290,7 +293,17 @@ func errHandler(err error, message string, exit bool) {
 // check the number of args
 func checkArg() {
     if len(os.Args) != 4  {     
-        fmt.Println("Usage: " + os.Args[0] + " <name> <port> <n>")  
+        fmt.Println("#Usage: " + os.Args[0] + " <name> <port> <n>")  
         os.Exit(0)  
     }
+}
+
+// check whether a slice of int contains an element
+func contains(s []int, e int) bool {
+    for _, a := range s {
+        if a == e {
+            return true
+        }
+    }
+    return false
 }
